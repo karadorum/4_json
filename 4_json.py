2557b8190d2bbf26001bcea9e370b41a851f70f1
Oleg Blinov <contextstudio78@gmail.com>
2018-06-19 20:37:04 +0300

    Update 4_json.py

--- 4_json.py
+++ 4_json.py
@@ -1,16 +1,23 @@
 import json
 
 
-def load_data():
-    with open('alco_shops.json', encoding='utf-8') as file:
-        parsed = json.load(file)
-    return parsed
+def load_data(filepath):
+    try:
+        with open(filepath, encoding='utf-8') as file:
+            try:
+                parsed_data = json.load(file)
+                return parsed_data
+            except json.JSONDecodeError:
+                print('file must be json')
+    except FileNotFoundError:
+        print('file not found')
 
 
-def pretty_print_json(json_data):
-    prettyfied = json.dumps(json_data, indent=4, sort_keys=True)
-    print(prettyfied)
+def pretty_print_json(raw_data):
+    prettyfied_text = json.dumps(raw_data, indent=4, sort_keys=True)
+    print(prettyfied_text)
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data())
+    filepath = input('Enter filepath: ')
+    pretty_print_json(load_data(filepath))
