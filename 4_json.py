660d506f2721faf210ede05e00cba17351aaa59f
Oleg Blinov <contextstudio78@gmail.com>
2018-06-25 23:12:57 +0300

    Added exceptions & argument function

--- 4_json.py
+++ 4_json.py
@@ -1,23 +1,24 @@
 import json
+import sys
 
 
-def load_data(filepath):
-    try:
-        with open(filepath, encoding='utf-8') as file:
-            try:
-                parsed_data = json.load(file)
-                return parsed_data
-            except json.JSONDecodeError:
-                print('file must be json')
-    except FileNotFoundError:
-        print('file not found')
+def load_data(filepath): 
+    with open(filepath, encoding='utf-8') as file:   
+        parsed_data = json.load(file)
+        return parsed_data
+            
 
 
-def pretty_print_json(raw_data):
-    prettyfied_text = json.dumps(raw_data, indent=4, sort_keys=True)
+def pretty_print_json(data):
+    prettyfied_text = json.dumps(data, indent=4, sort_keys=True)
     print(prettyfied_text)
 
 
 if __name__ == '__main__':
-    filepath = input('Enter filepath: ')
-    pretty_print_json(load_data(filepath))
+    
+    try:
+        pretty_print_json(load_data(sys.argv[1]))
+    except json.JSONDecodeError:
+        print('file must be json')
+    except FileNotFoundError:
+        print('file not found')
