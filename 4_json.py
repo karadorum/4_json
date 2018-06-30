7830e19869198fdf9b8958b0425b01e91bf7147f
Oleg Blinov <contextstudio78@gmail.com>
2018-06-30 13:06:27 +0300

    Added get_arg()

--- 4_json.py
+++ 4_json.py
@@ -2,23 +2,29 @@ import json
 import sys
 
 
-def load_data(filepath): 
-    with open(filepath, encoding='utf-8') as file:   
+def load_data(filepath):
+    with open(filepath, encoding='utf-8') as file:
         parsed_data = json.load(file)
         return parsed_data
-            
 
 
-def pretty_print_json(json_text):
-    prettyfied_text = json.dumps(json_text, indent=4, sort_keys=True)
+def pretty_print_json(data):
+    prettyfied_text = json.dumps(data, indent=4, sort_keys=True)
     print(prettyfied_text)
 
 
-if __name__ == '__main__':
-    
+def get_arg():
     try:
-        pretty_print_json(load_data(sys.argv[1]))
+        arg_data = load_data(sys.argv[1])
     except json.JSONDecodeError:
         print('file must be json')
     except FileNotFoundError:
         print('file not found')
+    except IndexError:
+        print('name of file argument is empty')
+    return arg_data
+
+
+if __name__ == '__main__':
+
+    pretty_print_json(get_arg())
